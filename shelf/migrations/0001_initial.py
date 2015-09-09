81d9c568e9abbe14d0d88d35f25ab130e9fda405
przemsto <przemyslaw.stopczanski@gmail.com>
2015-09-09 21:49:30 +0200

    added changes to apps

---
+++ shelf/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=20)),
+                ('last_name', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=100)),
+                ('isbn', models.CharField(max_length=17)),
+                ('author', models.ForeignKey(to='shelf.Author')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Publisher',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=70)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='publisher',
+            field=models.ForeignKey(to='shelf.Publisher'),
+        ),
+    ]
